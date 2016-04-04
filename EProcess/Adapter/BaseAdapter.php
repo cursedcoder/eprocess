e02736c3d90e85078fb3dda476396ddcd4a4c3d9
Yevhenii Huselietov <d46k16@gmail.com>
2016-04-04 21:54:04 +0800

    Use Runtime exceptions

--- EProcess/Adapter/BaseAdapter.php
+++ EProcess/Adapter/BaseAdapter.php
@@ -18,15 +18,15 @@ abstract class BaseAdapter
     protected function createUnixSocket()
     {
         if (!defined('EPROCESS_SOCKET_DIR')) {
-            throw new \Exception("EPROCESS_SOCKET_DIR is not defined.");
+            throw new \RuntimeException('EPROCESS_SOCKET_DIR is not defined.');
         }
 
         if (!defined('EPROCESS_AUTOLOAD')) {
-            throw new \Exception('EPROCESS_AUTOLOAD is not defined.');
+            throw new \RuntimeException('EPROCESS_AUTOLOAD is not defined.');
         }
 
         if (!is_writable(EPROCESS_SOCKET_DIR)) {
-            throw new \Exception(sprintf("Cannot write to %s.", EPROCESS_SOCKET_DIR));
+            throw new \RuntimeException(sprintf('Cannot write to "%s".', EPROCESS_SOCKET_DIR));
         }
 
         $unixFile = sprintf('%s/%s.sock', EPROCESS_SOCKET_DIR, $this->node);
